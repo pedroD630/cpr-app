fc2131ebd8711bde464f209f507031994f940d7f
PedroWin <87133537+pedroD630@users.noreply.github.com>
2024-12-15 17:35:34 -0300

    feat: Reutilizar menu inferior

---
+++ src/script/script.js
@@ -0,0 +1,12 @@
+// Carrega o menu HTML dinamicamente
+fetch('/src/reutil/menu-inf.html')
+  .then(response => {
+    if (!response.ok) throw new Error(`Erro ao carregar menu: ${response.statusText}`);
+    return response.text(); // Converte o conteúdo para texto
+  })
+  .then(menuHTML => {
+    document.getElementById('menu').innerHTML = menuHTML; // Insere o menu no elemento com id="menu"
+  })
+  .catch(error => {
+    console.error('Erro ao carregar o menu:', error); // Exibe o erro no console
+  });
