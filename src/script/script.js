5ef1bd517fdcf03b80ccc06fc5cfae5da26603ac
PedroWin <87133537+pedroD630@users.noreply.github.com>
2024-12-25 17:31:09 -0300

    feat: Implementar geração de PDFs para reunião e partes
    - Adicionar funcionalidade para criar PDFs com detalhes da reunião e suas partes.
    - Configurar layout personalizado no pdfmake, incluindo tempos e descrições.
    - Suporte a orientações (landscape/retrato) e tamanhos personalizados de página.
    - Gerar PDF diretamente com os dados extraídos do HTML.

--- src/script/script.js
+++ src/script/script.js
@@ -209,7 +209,112 @@ function gerarPdfMds(){
   const horaFim = document.getElementById("horaFim").value;
   const tempoTotal = document.getElementById("tempoTotal").value;
 
+  // playground requires you to assign document definition to a variable called dd
+  var dd = {
+    pageSize: 'A4',
+    pageOrientation: 'landscape',
+    content: [
+      { text: 'Reunião Meio de Semana', style: 'subheader' },
+      {
+        style: 'tableExample',
+        table: {
+          widths: ['auto', 'auto'], // Define as larguras das colunas
+          body: [
+            ['Hora Início', `${horaInicio}`],
+          ],
+        },
+      },
+      {
+        style: 'tableExample',
+        color: '#444',
+        table: {
+          widths: ['auto', 'auto', 'auto', 'auto'], // Larguras para 4 colunas
+          headerRows: 1, // Define o número de linhas de cabeçalho
+          body: [
+            // Primeira linha com colSpan
+            [
+              { text: '', style: 'tableHeader', alignment: 'center' }, // Cabeçalho vazio
+              { text: 'Parte', style: 'tableHeader', alignment: 'center' },
+              { text: 'Tempo Gasto', style: 'tableHeader', alignment: 'center' },
+              { text: 'Quem', style: 'tableHeader', alignment: 'center' }
+            ],
+            // Segunda linha de cabeçalho
+            ['', { text: 'Comentários Iniciais', alignment: 'center' }, `${comentInicial.tempo}`, `${comentInicial.quem}`],
+            // Corpo da tabela
+            [
+              { rowSpan: 4, text: 'Tesouros da Palavra de Deus', fillColor: '#cce5ff' },
+              'Discurso (10 min.)',
+              `${discurso.tempo}`,
+              `${discurso.quem}`
+            ],
+            ['', 'Encontre Joias (10 min.)', `${encontreJoias.tempo}`, `${encontreJoias.quem}`],
+            ['', 'Leitura da Bíblia (4 min.)', `${leituraBiblia.tempo}`, `${leituraBiblia.quem}`],
+            ['', 'Conselho (1 min.)', `${conselho1.tempo}`, `${conselho1.quem}`],
+            [
+              { rowSpan: 8, text: 'Faça Seu Melhor no Ministério', fillColor: '#ffda9e'},
+              '1° Parte',
+              `${parte1.tempo}`,
+              `${parte1.quem}`
+            ],
+            ['', 'Conselho (1 min.)', `${conselho2.tempo}`, `${conselho2.quem}`],
+            ['', '2° Parte', `${parte2.tempo}`, `${parte2.quem}`],
+            ['', 'Conselho (1 min.)', `${conselho3.tempo}`, `${conselho3.quem}`],
+            ['', '3° Parte', `${parte3.tempo}`, `${parte3.quem}`],
+            ['', 'Conselho (1 min.)', `${conselho4.tempo}`, `${conselho4.quem}`],
+            ['', '4° Parte', `${parte4.tempo}`, `${parte4.quem}`],
+            ['', 'Conselho (1 min.)', `${conselho5.tempo}`, `${conselho5.quem}`],
+            [
+              { rowSpan: 4, text: 'Nossa Vida Cristã', fillColor: '#fcb7af' },
+              '1° Parte',
+              `${NvcParte1.tempo}`,
+              `${NvcParte1.quem}`
+            ],
+            ['', '2° Parte', `${NvcParte2.tempo}`, `${NvcParte2.quem}`],
+            ['', '3° Parte', `${NvcParte3.tempo}`, `${NvcParte3.quem}`],
+            ['', 'Estudo Bíblico', `${estudoBiblico.tempo}`, `${estudoBiblico.quem}`],
+
+            ['', 'Comentários Finais', `${comentariosFinais.tempo}`, `${comentariosFinais.quem}`],
+            ['', 'Anúncios', `${anuncios.tempo}`, `${anuncios.quem}`],
+          ],
+        },
+      },
+      {
+        style: 'tableExample',
+        table: {
+          widths: ['auto', 'auto', 'auto', 'auto'], // Define as larguras das colunas
+          body: [
+            [{text: 'Hora Fim', fillColor: '#fbdcb0'}, `${horaFim}`, {text:'Tempo Total', fillColor: '#a4d9a3'}, `${tempoTotal}`]
+          ],
+        },
+      },
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true,
+        margin: [0, 0, 0, 10],
+      },
+      subheader: {
+        fontSize: 16,
+        bold: true,
+        margin: [0, 10, 0, 5],
+        alignment: 'center',
+      },
+      tableExample: {
+        margin: [0, 5, 0, 15],
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 13,
+        color: 'black',
+      },
+    },
+    defaultStyle: {
+      // alignment: 'justify'
+    },
+  };
   
+  pdfMake.createPdf(dd).download('nome-do-arquivo.pdf');
 }
 
 function gerarPdfRelatorio(tipoReuniao){
