b6256fbe2f75cd39d216ae0e9b8d7830b2306715
PedroWin <87133537+pedroD630@users.noreply.github.com>
2025-03-05 09:26:39 -0300

    Remove urls para github pages

--- src/script/script.js
+++ src/script/script.js
@@ -1,4 +1,7 @@
-const baseUrl = "https://pedrod630.github.io/kairos-manager-app/";
+import { carregarCronometro, carregarMenu } from "./ui.js";
+
+const baseUrl = "/";
+
 let cron;
 let startTime;
 let elapsedTime = 0;
@@ -28,30 +31,8 @@ class Parte {
   }
 }
 
-// Carrega o menu HTML dinamicamente
-fetch(`${baseUrl}src/reutil/menu-inf.html`)
-  .then(response => {
-    if (!response.ok) throw new Error(`Erro ao carregar menu: ${response.statusText}`);
-    return response.text(); // Converte o conteúdo para texto
-  })
-  .then(menuHTML => {
-    document.getElementById('menu').innerHTML = menuHTML; // Insere o menu no elemento com id="menu"
-  })
-  .catch(error => {
-    console.error('Erro ao carregar o menu:', error); // Exibe o erro no console
-  });
-
-fetch(`${baseUrl}src/reutil/cronometro.html`)
-  .then(response => {
-    if (!response.ok) throw new Error(`Erro ao carregar cronometro: ${response.statusText}`);
-    return response.text(); // Converte o conteúdo para texto
-  })
-  .then(menuHTML => {
-    document.getElementById('cronometro').innerHTML = menuHTML; // Insere o menu no elemento com id="menu"
-  })
-  .catch(error => {
-    console.error('Erro ao carregar o cronometro:', error); // Exibe o erro no console
-  });
+carregarCronometro(baseUrl);
+carregarMenu(baseUrl);
 
 function selecionarRelPorData() {
   const diasFimDeSemana = [5, 6, 0];
@@ -488,3 +469,5 @@ document.addEventListener("DOMContentLoaded", () =>{
     }
   });
 });
+
+window.selecionarRelPorData = selecionarRelPorData;
