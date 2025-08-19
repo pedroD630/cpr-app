6283a8460e682d65d5d0fdd1dac0133fb0cf7754
Pedro <pedro@tridata.com.br>
2025-08-19 15:01:35 -0300

    feat: converte o tempo de strings para um objeto com horas, minutos e segundos

--- src/script/utils.js
+++ src/script/utils.js
@@ -14,3 +14,26 @@ export function receberHorario() {
 export function returnData(input) {
     return input < 10 ? `0${input}` : input;
 }
+
+// converte o tempo de strings para um objeto com horas, minutos e segundos
+export function parseTempo(tempoStr) {
+  if (!tempoStr) return { horas: false, minutos: 0, segundos: 0 };
+
+  let partes = tempoStr.split(":").map(Number);
+
+  let horas = 0;
+  let minutos = 0;
+  let segundos = 0;
+
+  if (partes.length === 2) {
+    minutos = partes[0];
+    segundos = partes[1];
+  } else if (partes.length === 3) {
+    // formato HH:MM:SS
+    horas = partes[0];
+    minutos = partes[1];
+    segundos = partes[2];
+  }
+
+  return { horas, minutos, segundos };
+}
